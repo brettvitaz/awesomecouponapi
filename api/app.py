8a0c3fb750027de918f6b197aa310ad3094ffe99
Brett Vitaz <vitazbt@uw.edu>
2017-03-01 21:50:21 -0800

    add function to create app and setup database. use waitress to serve flask wsgi app

--- api/app.py
+++ api/app.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from waitress import serve
 
-app = Flask(__name__)
+from api import config
+from api.importer import import_data
+from api.models import db, Coupons, Stores
+from api.schemas import ma, CouponsSchema, StoresSchema
+
+
+def create_app(config_module=None):
+    app = Flask(__name__)
+    app.config.from_object(config_module)
+
+    db.init_app(app)
+    ma.init_app(app)
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        if not Coupons.query.all():
+            import_data(db)
+
+    return app
+
+
+app = create_app(config)
 
 
 @app.route('/')
 def route_root():
     return 'Coupon API'
 
-app.run(host='0.0.0.0')
+
+serve(app, host='0.0.0.0', port='5000')
