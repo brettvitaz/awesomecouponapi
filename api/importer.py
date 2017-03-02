77203b8d17977a8b212269ae8ef43477fc402be6
Brett Vitaz <vitazbt@uw.edu>
2017-03-01 21:47:25 -0800

    add import_data function

---
+++ api/importer.py
@@ -0,0 +1,16 @@
+import simplejson as json
+
+from api.schemas import CouponsSchema
+
+coupons_schema = CouponsSchema(many=True)
+
+
+def import_data(db):
+    with open('../coupons.json') as f:
+        j = json.load(f)
+        coupon = coupons_schema.load(j)
+        try:
+            db.session.add_all(coupon.data)
+            db.session.commit()
+        finally:
+            db.session.close()
