e66c0ebb7886f42fda8086c2039b1f08621cd2fd
Brett Vitaz <vitazbt@uw.edu>
2017-03-03 07:27:32 -0800

    extend coupon schema to allow reading the full date on import

--- api/importer.py
+++ api/importer.py
@@ -1,12 +1,19 @@
 import simplejson as json
 
-from api.schemas import CouponsSchema
+from api.schemas import ma, CouponsSchema
 
-coupons_schema = CouponsSchema(many=True)
+
+class CouponsImportSchema(CouponsSchema):
+    expire_at = ma.String()
+    published_at = ma.String()
+
+
+coupons_schema = CouponsImportSchema(many=True)
 
 
 def import_data(db):
-    with open('../coupons.json') as f:
+    print('importing...')
+    with open('coupons.json') as f:
         j = json.load(f)
         coupon = coupons_schema.load(j)
         try:
