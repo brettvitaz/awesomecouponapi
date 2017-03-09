5678fd730c45d2b73d19fefeab801dd6cda2f95c
Brett Vitaz <vitazbt@uw.edu>
2017-03-08 20:47:48 -0800

    add a few more tests

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -22,6 +22,12 @@ class TestCouponDateTime:
             result = self.field.deserialize(value)
         assert 'does not match format' in str(e.value)
 
+    def test_deserialize_fail_none(self):
+        value = None
+        with pytest.raises(ValidationError) as e:
+            result = self.field.deserialize(value)
+        assert 'Field may not be null.' in str(e.value)
+
     def test_serialize(self):
         expected = '2016-08-05T08:40:51.620Z'
 
@@ -31,9 +37,17 @@ class TestCouponDateTime:
         result = self.field.serialize('value', obj)
         assert result == expected
 
-    def test_serialize_fail(self):
+    def test_serialize_fail_none(self):
         obj = {
             'value': None
+        }
+        with pytest.raises(ValidationError) as e:
+            result = self.field.serialize('value', obj)
+        assert 'Not a valid datetime.' in str(e.value)
+
+    def test_serialize_fail_attr_err(self):
+        obj = {
+            'value': object
         }
         with pytest.raises(ValidationError) as e:
             result = self.field.serialize('value', obj)
